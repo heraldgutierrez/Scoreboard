144735cb25b22955889b966375fda4bc93829ddd
Herald Gutierrez <heraldgutierrez@hotmail.com>
2014-02-14 13:51:49 -0600

    redirect "/" to portfolio/scoreboard

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@
  */
 
 exports.index = function(req, res){
-	res.render('scoreboard_led');
+	// res.render('scoreboard_led');
+	res.redirect('http://heraldgutierrez.com/Portfolio/Scoreboard');
 };
 
 exports.scoreboard = function(req, res) {
